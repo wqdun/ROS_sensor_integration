761b2af4ef02133fa61ca2961ae507371a2e1993
dunwenqiang <dunwenqiang@navinfo.com>
2017-11-11 11:15:27 +0800

    【修改】读取文件功能开发，经纬度转换为WGS84坐标系；Marker显示的测试，TODO：显示实际轨迹。

--- SmartCollect/src/public_tools/public_tools.h
+++ SmartCollect/src/public_tools/public_tools.h
@@ -1,19 +1,34 @@
 #ifndef PUBLIC_TOOLS_H
 #define PUBLIC_TOOLS_H
 
+#include <ros/ros.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <visualization_msgs/Marker.h>
+
+namespace public_tools
+{
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} pointXYZ_t;
 
 class PublicTools {
 public:
 
-    static void tf_example();
-
-
+    static int string2int(const string& str);
+    static void GeoToGauss(double jd, double wd, short DH, short DH_width, double *y, double *x, double LP);
 
 };
 
 
-
-
+}
+// namespace public_tools
 
 #endif

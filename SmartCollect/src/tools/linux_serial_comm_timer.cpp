8fbebfce21fd1b9583b40a04cc1f503d649800d7
dunwenqiang <dunwenqiang@navinfo.com>
2017-12-04 19:33:24 +0800

    这个版本可以读取串口，需要安装驱动，64位：http://www.ftdichip.com/Drivers/D2XX.htm

--- SmartCollect/src/tools/linux_serial_comm_timer.cpp
+++ SmartCollect/src/tools/linux_serial_comm_timer.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -20,8 +20,7 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
-
-static double string2double(const string& str);
+using namespace std;
 
 int set_opt(int fd, int nSpeed, int nBits, char nEvent, int nStop) {
     struct termios newtio, oldtio;
@@ -112,110 +111,64 @@ int set_opt(int fd, int nSpeed, int nBits, char nEvent, int nStop) {
 }
 
 int main(int argc, char **argv) {
-    char buf[1024];
-
     // open serial port: ttyS0
     if(argc < 2) {
         cout << "Wrong arg, arg shouble be /dev/tty..." << endl;
         exit(1);
     }
     cout << argv[1] << endl;
-    int fd1 = open(argv[1], O_RDONLY | O_NONBLOCK); // 打开串口 // fd1 = open("/dev/ttyUSB0", O_RDWR);
-    if(-1 == fd1) {
-        printf("fd1 == -1\n");
+    int fd = open(argv[1], O_RDONLY); //  | O_NONBLOCK); // 打开串口 // fd = open("/dev/ttyUSB0", O_RDWR);
+    if(-1 == fd) {
+        printf("Serial port error\n");
         exit(1);
     }
+    cout << "Open " << ttyname(fd) << " successfully\n";
 
     // setup port properties
-    int nset1 = set_opt(fd1, 115200, 8, 'N', 1); // 设置串口属性
+    int nset1 = set_opt(fd, 115200, 8, 'N', 1); // 设置串口属性
     if(-1 == nset1) {
         exit(1);
     }
+    cout << "Set " << ttyname(fd) << " successfully\n";
 
-    std::string frameBuf;
-    long freq = 0;
-    timespec t1, t2;
-    long last_time_ns;
-    long time_when_get_frame_s, time_when_get_frame_ns;
-    double time_s;
 
-    int last_minute = 0;
+    FILE *pOutFile;
+    if(!(pOutFile = fopen("5651_422.dat", "wb") ) ) {
+        cout << "Create file: failed.";
+        exit(1);
+    }
+    cout << "Create file successfully: 5651_422.dat";
+    unsigned char buf[50];
+    fd_set rd;
+    int nread = 0;
+    string frameBuf("");
 
     while(1) {
-        memset(buf, 0, 1024);
-        int nread = read(fd1, buf, 1024); // 读串口
-        if(nread <= 0) {
+        nread = read(fd, buf, sizeof(buf) );
+        // string bufStr(buf);
+        // fill_frame(bufStr, frameBuf);
+        // now frameBuf should be human readable
+
+
+        // parse
+        // (void)fwrite(buf, nread, 1, pOutFile); // << C txt
+        // if() set a member; then pub -> infor
+
+        // cout << dec << "nread: " << nread << "\n";
+        if(nread <= 0)
+            // break;
             continue;
+
+        for(int k = 0; k < nread; ++k) {
+            printf("%02X ", buf[k]);
+            // cout << hex << (int)buf[k]; // or nread
         }
-
-        for(size_t i = 0; i < nread; ++i) {
-            bool is_frame_completed = false;
-            string frame_complete = "";
-            switch(buf[i]) {
-            case '$':
-                clock_gettime(CLOCK_REALTIME, &t2);
-                // cout << "time_end  :" << t2.tv_nsec << endl;
-                time_when_get_frame_s = t2.tv_sec;
-                time_when_get_frame_ns = t2.tv_nsec;
-                time_s = (double)time_when_get_frame_s + (double)time_when_get_frame_ns / 1000000000.0;
-                // freq = 1000000000 / (t2.tv_nsec - last_time_ns);
-                // cout << "The Freq is: " << freq << endl;
-                frameBuf = buf[i];
-
-                // clock_gettime(CLOCK_MONOTONIC, &t1);
-                // cout << "time_start:" << t1.tv_nsec << endl;
-                // last_time_ns = t1.tv_nsec;
-                break;
-            case '\r':
-                break;
-            case '\n':
-                time_when_get_frame_s %= 3600;
-                cout << "[" << time_when_get_frame_s / 60 << ":" << time_when_get_frame_s % 60 << "]:"
-                     << "[" << std::fixed << time_s << "]"
-                     << frameBuf << endl;
-                is_frame_completed = true;
-                frame_complete = frameBuf;
-                frameBuf.clear();
-                break;
-            default:
-                frameBuf += buf[i];
-            }
-
-            if(is_frame_completed) {
-                vector<string> parsed_data;
-                // parse the GPS time
-                boost::split(parsed_data, frame_complete, boost::is_any_of( ",*" ), boost::token_compress_on);
-                if(parsed_data.size() <= 16) {
-                    // 1st frame might be wrong, 'cause no '$'
-                    cout << "Error when parsing." << parsed_data.size() << endl;
-                    continue;
-                }
-
-                // e.g. "279267.900"
-                string GPS_week_time_str = parsed_data[2];
-                double GPS_week_time = string2double(GPS_week_time_str);
-                int hour = (int)GPS_week_time / (60 * 60) % 24;
-                int minute = (int)GPS_week_time / 60 % 60;
-                double second = fmod(GPS_week_time, 60);
-
-                if(minute != last_minute) {
-                    char command[100] = "";
-                    sprintf(command, "date -s \"%d:%d:%f\"", hour, minute, second);
-                    system(command);
-                    cout << "Modify Unix time successfully." << endl;
-                }
-                last_minute = minute;
-            }
-        }
+        // cout << "\n";
+        bzero(buf, sizeof(buf) );
     }
 
-    close(fd1);
-    return 0;
-}
+    fclose(pOutFile);
 
-static double string2double(const string& str) {
-    std::istringstream iss(str);
-    double num;
-    iss >> num;
-    return num;
+    close(fd);
+    return 0;
 }

16728f0ea5aed07a3cb8c1257f3017325261a6e4
dunwenqiang <dunwenqiang@navinfo.com>
2017-12-15 16:00:51 +0800

    【测试】模版函数实现atoi；增加了空值判断。

--- SmartCollect/src/tools/test.cc
+++ SmartCollect/src/tools/test.cc
@@ -11,14 +11,28 @@
 
 using namespace std;
 
-double string2double(const string& str) {
+template <typename T>
+T string2num(const string& str, const T retIfEmpty) {
+    if(str.empty() ) {
+        cout << __FUNCTION__ << " param is null.";
+        return retIfEmpty;
+    }
     std::istringstream iss(str);
-    double num;
+    T num;
     iss >> num;
     return num;
 }
-int main(int argc, char **argv) {
 
-    cout << string2double("0") << endl;
+double weekSec2DaySec(const double weekSec) {
+    // +8: Beijing Time
+    double dayHour = (int)weekSec / 3600 % 24 + 8;
+    double dayMinute = (int)weekSec / 60 % 60;
+    double daySecond = fmod(weekSec, 3600 * 24);
+    cout << daySecond << endl;
+}
+
+int main(int argc, char **argv) {
+    cout << fixed << string2num("123.564", .0) << endl;
+    // cout << weekSec2DaySec(279267.900) << endl;
 
 }

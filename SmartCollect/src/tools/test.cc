bd00b52f75e5d0e546f975d78cd03f669220658e
dunwenqiang <dunwenqiang@navinfo.com>
2018-03-08 14:31:05 +0800

    复杂类型对象作为参数来创建线程；lexical_cast的使用； 参考http://blog.163.com/niwei_258/blog/static/10628488201081594152598/

--- SmartCollect/src/tools/test.cc
+++ SmartCollect/src/tools/test.cc
@@ -30,44 +30,82 @@ using namespace std;
 #define LOG(INFO) cout
 // #define LOG(ERROR) cerr
 
-#include <string>
 #include <iostream>
-#include <fstream>
+#include <boost/thread.hpp>
+
 using namespace std;
+using namespace boost;
 
-#include "rapidjson/rapidjson.h"
-#include "rapidjson/document.h"
-#include "rapidjson/reader.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
+#define BOOST_DATE_TIME_SOURCE
+#define BOOST_THREAD_NO_LIB
 
-using namespace rapidjson;
+
+//----------------------------------
+//函数声明
+// g++ test.cc -lboost_system -lboost_thread
+//start from the very beginning,and to create greatness
+//@author: Chuangwei Lin
+//@E-mail：979951191@qq.com
+//@brief： boost多线程编译的示例
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/lexical_cast.hpp>
 #include <iostream>
-#include <string>
-#include <fstream>
-//包含rapidjson必要头文件,rapidjson文件夹拷贝到工程目录，或者设置include路径，或者加入到工程树
-#include "rapidjson/document.h"
-#include "rapidjson/prettywriter.h"
-#include "rapidjson/stringbuffer.h"
-#include "rapidjson/filereadstream.h"
-#include "rapidjson/ostreamwrapper.h"
-#include "rapidjson/istreamwrapper.h"
-#include "rapidjson/writer.h"
-#include <fstream>
 
+boost::mutex io_mutex;
 
-void core_test1()
+struct cccccc
 {
-    int i = 0;
-    //below will call segmentfault
-    scanf("%d", i);
-    printf("%d", i);
+        cccccc(int id) : id(id) { }
 
+        void operator()()
+        {
+            boost::mutex::scoped_lock lock(io_mutex);
+                for (int i = 0; i < 10; ++i)
+                {
+                        // boost::mutex::scoped_lock lock(io_mutex);
+                        std::cout << id << ": "
+                        << i << std::endl;
+                }
+        }
+
+        int id;
+};
+
+
+void lcw1()
+{
+
+    std::cout << "lcw1 is working!" << std::endl;
 }
 
-
-int main()
+void lcw2()
 {
-    core_test1();
+
+    std::cout << "lcw2 is working!" << std::endl;
+}
+
+int main (int argc, char ** argv)
+{
+    using namespace std;
+
+    using namespace boost;
+    using boost::lexical_cast;
+    thread thread_1 = thread(lcw1);
+    thread thread_2 = thread(lcw2);
+
+    boost::thread thrd1(cccccc(1));
+    boost::thread thrd2(cccccc(2));
+
+    thrd1.join();
+    thrd2.join();
+
+    thread_2.join();
+    thread_1.join();
+
+    int a = lexical_cast<int>("123");
+    double b = lexical_cast<double>("");
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
     return 0;
 }

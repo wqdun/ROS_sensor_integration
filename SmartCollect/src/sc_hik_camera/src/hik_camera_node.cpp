cf0ebfefdf12a9b586c3b5d28f9a4cb85679241d
dunwenqiang <dunwenqiang@navinfo.com>
2018-11-08 17:27:40 +0800

    【修改】首次catkin编译通过的。

--- SmartCollect/src/sc_hik_camera/src/hik_camera_node.cpp
+++ SmartCollect/src/sc_hik_camera/src/hik_camera_node.cpp
@@ -1,16 +1,17 @@
-#include "hik_camera_arm.h"
+#include "hik_camera.h"
 
 #define NDEBUG
 // #undef NDEBUG
 #include <glog/logging.h>
 
 int main(int argc, char **argv) {
-    google::InitGoogleLogging(argv[0]);
+    // google::InitGoogleLogging(argv[0]);
     LOG(INFO) << "Got " << argc << " parameters.";
 
-    HikCamera hikCamera(isNodeRunning);
+    HikCamera hikCamera;
     hikCamera.Run();
     LOG(INFO) << "Returning...";
     return 0;
 }
 
+// sudo g++ -std=c++11 hik_camera_node.cpp hik_camera.cpp -lglog -L/opt/MVS/bin -lMvCameraControl -lopencv_core -lopencv_highgui -lopencv_imgproc -Wl,-rpath,/opt/MVS/bin:/usr/local/lib/

340060c437a8fb972f98a9d0abb3521cda65e1a3
dunwenqiang <dunwenqiang@navinfo.com>
2018-11-08 10:59:27 +0800

    【修改】更名，去掉arm，当前为amd64 ubuntu版本。

--- SmartCollect/src/sc_hik_camera/src/hik_camera_node.cpp
+++ SmartCollect/src/sc_hik_camera/src/hik_camera_node.cpp
@@ -4,17 +4,9 @@
 // #undef NDEBUG
 #include <glog/logging.h>
 
-
-bool isNodeRunning = true;
-void CtrlCHandler(int signo) {
-    LOG(INFO) << "Receive a Control+C.";
-    isNodeRunning = false;
-}
-
 int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
     LOG(INFO) << "Got " << argc << " parameters.";
-    // signal(SIGINT, CtrlCHandler);
 
     HikCamera hikCamera(isNodeRunning);
     hikCamera.Run();

22dbb547d9221d0880a28125f29e65ab06b30f07
dunwenqiang <dunwenqiang@gmail.com>
2019-01-08 11:19:16 +0800

    【test】使用settimeofday设置系统时间。

--- SmartCollect/src/tools/test.cc
+++ SmartCollect/src/tools/test.cc
@@ -1,42 +1,47 @@
 #include <iostream>
-#include <sys/stat.h>
+#include <ctime>
 #include <string>
-#include <vector>
+#include <sys/time.h>
+#include <time.h>
+
+using namespace std;
+
+// std::string unixTime2String(uint64_t unixTime) {
+//     tm tmNow = { 0 };
+//     localtime_r((time_t *)&unixTime, &tmNow);
 
 
-double file_size2(char* filename)
+//     cout << tmNow.tm_hour * 100 + tmNow.tm_min;
+//     // strftime(strTime, bufLen - 1, "%Y-%m-%d %H:%M:%S", &tmNow);
+//     // strTime[bufLen - 1] = '\0';
+// }
+
+int main(void)
 {
-    if(1) {
-        return 0;
-    }
-    struct stat statbuf;
-    stat(filename, &statbuf);
-    off_t sizeInByte = statbuf.st_size;
-    std::cout << sizeInByte << "B\n";
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday (&tv, &tz);
 
-    double sizeInKByte = (sizeInByte >> 10);
-    std::cout << std::fixed << sizeInKByte << "KB\n";
+    tv.tv_sec = 1546910000;
+    tv.tv_usec = 0;
 
-    double sizeInMByte = (sizeInByte >> 20);
-    std::cout << std::fixed << sizeInMByte << "MB\n";
+    settimeofday(&tv, NULL);
 
-    return sizeInMByte;
-}
-
-int getFilesInDir(std::vector<std::string> &files) {
-    if(NULL == files) {
-        std::cout << "NULL\n";
-    }
-    else {
-        std::cout << "NOT NULL\n";
-    }
     return 0;
 }
 
 
-int main(int argc, char *argv[])
-{
-    std::vector<std::string> _files;
-    getFilesInDir(_files);
-    return 0;
-}
+// time_t tt = time(NULL);
+//     tm *t= localtime(&tt);
+//     char nowTime[50];
+//     (void)sprintf(nowTime, "%02d%02d%02d", t->tm_hour, t->tm_min, t->tm_sec);
+//     fileName += nowTime;
+
+//     time_t now = time(NULL);
+//   tm tmNow = { 0 };
+//   localtime_r(&now, &tmNow);
+//   const int nowMinute = tmNow.tm_hour * 100 + tmNow.tm_min;
+//   if(nowMinute != lastMinute_) {
+//     lastMinute_ = nowMinute;
+//     isGonnaCreateNewLidarFile = true;
+//   }

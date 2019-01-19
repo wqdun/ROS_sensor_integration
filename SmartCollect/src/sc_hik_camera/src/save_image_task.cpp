44ee619ca6fdbece13258e86183e61f0162b8837
dunwenqiang <dunwenqiang@gmail.com>
2019-01-19 15:37:51 +0800

    【修改】记录保存一张照片的时间。

--- SmartCollect/src/sc_hik_camera/src/save_image_task.cpp
+++ SmartCollect/src/sc_hik_camera/src/save_image_task.cpp
@@ -1,10 +1,11 @@
 #include "save_image_task.h"
 
 SaveImageTask::SaveImageTask(const cv::Mat &_image, const MV_FRAME_OUT_INFO_EX &_frameInfo, const std::string &_picFile) {
-    DLOG(INFO) << __FUNCTION__ << " start.";
+    LOG_EVERY_N(INFO, 100) << __FUNCTION__ << " start.";
     image_ = _image;
     frameInfo_ = _frameInfo;
     picFile_ = _picFile;
+    LOG_EVERY_N(INFO, 100) << __FUNCTION__ << " end.";
 }
 
 SaveImageTask::~SaveImageTask() {
@@ -12,8 +13,8 @@ SaveImageTask::~SaveImageTask() {
 }
 
 void SaveImageTask::doit() {
-    DLOG(INFO) << __FUNCTION__ << " start.";
+    LOG_EVERY_N(INFO, 100) << __FUNCTION__ << " start.";
     cv::imwrite(picFile_, image_);
-    DLOG(INFO) << "Save " << picFile_;
+    LOG_EVERY_N(INFO, 100) << "Save " << picFile_;
 }
 

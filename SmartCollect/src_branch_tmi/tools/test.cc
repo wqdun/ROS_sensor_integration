07966a14c7b530fa743e75078bf55b24c67a6654
dunwenqiang <dunwenqiang@gmail.com>
2019-04-04 20:25:13 +0800

    【修改】topcoder: AdditionCycles::cycleLength

--- SmartCollect/src_branch_tmi/tools/test.cc
+++ SmartCollect/src_branch_tmi/tools/test.cc
@@ -1,46 +1,52 @@
 #include <iostream>
 #include <sstream>
 
-int Get1st(int inNum) {
-    return inNum / 10;
-}
+class AdditionCycles {
+public:
+    int Get1st(int inNum) {
+        return inNum / 10;
+    }
 
-int Get2nd(int inNum) {
-    return inNum % 10;
-}
+    int Get2nd(int inNum) {
+        return inNum % 10;
+    }
 
+    int GetDigitSum(int inNum) {
+        return Get1st(inNum) + Get2nd(inNum);
+    }
 
-int GetDigitSum(int inNum) {
-    return Get1st(inNum) + Get2nd(inNum);
-}
+    int String2Int(const std::string& str) {
+        if(str.empty() ) {
+            return -1;
+        }
+        std::istringstream iss(str);
+        int num;
+        iss >> num;
+        return num;
+    }
 
-int String2Int(const std::string& str) {
-    if(str.empty() ) {
+    int cycleLength(int inNum) {
+        const int origin = inNum;
+        int _inNum = origin;
+
+        int counter = 0;
+        while (true) {
+            ++counter;
+            int digitSum = GetDigitSum(_inNum);
+            _inNum = 10 * Get2nd(_inNum) + Get2nd(digitSum);
+            // std::cout << _inNum << "\n";
+            if (_inNum == origin) {
+                std::cout << counter << "\n";
+                return counter;
+            }
+        }
+
+        // std::cout << "Bye...\n";
         return -1;
     }
-    std::istringstream iss(str);
-    int num;
-    iss >> num;
-    return num;
+
 }
 
-int main(int argc, char *argv[]) {
-    const int origin = String2Int(argv[1]);
-    int inNum = origin;
 
-    int counter = 0;
-    while (true) {
-        ++counter;
-        int digitSum = GetDigitSum(inNum);
-        inNum = 10 * Get2nd(inNum) + Get2nd(digitSum);
-        // std::cout << inNum << "\n";
-        if (inNum == origin) {
-            std::cout << counter << "\n";
-            return counter;
-        }
-    }
 
-    // std::cout << "Bye...\n";
-    return -1;
-}
 

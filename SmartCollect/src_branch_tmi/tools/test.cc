08e87632cce9067ef6bcafebf7facce1e9701dea
dunwenqiang <dunwenqiang@gmail.com>
2019-04-04 20:14:08 +0800

    【修改】topcoder: AdditionCycles::cycleLength

--- SmartCollect/src_branch_tmi/tools/test.cc
+++ SmartCollect/src_branch_tmi/tools/test.cc
@@ -1,37 +1,46 @@
 #include <iostream>
-#include <deque>
-#include <algorithm>
-#include <string>
-#include <map>
+#include <sstream>
 
-using namespace std;
-
-
-
-void sort_stdalg(deque<double> _dq) {
-    std::sort(_dq.begin(), _dq.end());
+int Get1st(int inNum) {
+    return inNum / 10;
 }
 
-void print_deque(deque<double> &_dq) {
-    for(size_t i =0; i < _dq.size(); ++i) {
-        std::cout << i << ": " << _dq[i] << "\n";
+int Get2nd(int inNum) {
+    return inNum % 10;
+}
+
+
+int GetDigitSum(int inNum) {
+    return Get1st(inNum) + Get2nd(inNum);
+}
+
+int String2Int(const std::string& str) {
+    if(str.empty() ) {
+        return -1;
     }
+    std::istringstream iss(str);
+    int num;
+    iss >> num;
+    return num;
 }
 
+int main(int argc, char *argv[]) {
+    const int origin = String2Int(argv[1]);
+    int inNum = origin;
 
-int main(int argc, char const *argv[]) {
-    std::deque<double> dq {1, 3, 2, 4, 0, 3.2, 3.6};
-    std::deque<double> _dq(dq);
-    print_deque(dq);
-    sort_stdalg(dq);
-    print_deque(dq);
-    print_deque(_dq);
-    std::cout << _dq[dq.size() / 2] << "\n";
-    std::cout << dq[dq.size() / 2] << "\n";
-    return 0;
+    int counter = 0;
+    while (true) {
+        ++counter;
+        int digitSum = GetDigitSum(inNum);
+        inNum = 10 * Get2nd(inNum) + Get2nd(digitSum);
+        // std::cout << inNum << "\n";
+        if (inNum == origin) {
+            std::cout << counter << "\n";
+            return counter;
+        }
+    }
+
+    // std::cout << "Bye...\n";
+    return -1;
 }
-
-
-
-
 

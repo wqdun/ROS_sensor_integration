64b0f1ee072c45741c26cccf791e3c2e03ecc1ec
dunwenqiang <dunwenqiang@gmail.com>
2019-04-10 11:28:15 +0800

    【修改】test.cc

--- SmartCollect/src_branch_tmi/tools/test.cc
+++ SmartCollect/src_branch_tmi/tools/test.cc
@@ -1,52 +1,78 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
 
-class AdditionCycles {
-public:
-    int Get1st(int inNum) {
-        return inNum / 10;
+
+struct NodeList {
+    int data;
+    NodeList *next;
+
+    NodeList(): data(0), next(NULL) {
     }
-
-    int Get2nd(int inNum) {
-        return inNum % 10;
+    NodeList(int i): data(i), next(NULL) {
     }
+};
 
-    int GetDigitSum(int inNum) {
-        return Get1st(inNum) + Get2nd(inNum);
-    }
 
-    int String2Int(const std::string& str) {
-        if(str.empty() ) {
-            return -1;
+int GetMinIndex(const std::vector<int> &inArr, int startIndex, int endIndex) {
+    int minIndex = startIndex;
+    for (int i = startIndex + 1; i <= endIndex; ++i) {
+        if (inArr[i] < inArr[minIndex]) {
+            minIndex = i;
         }
-        std::istringstream iss(str);
-        int num;
-        iss >> num;
-        return num;
     }
 
-    int cycleLength(int inNum) {
-        const int origin = inNum;
-        int _inNum = origin;
+    return minIndex;
+}
 
-        int counter = 0;
-        while (true) {
-            ++counter;
-            int digitSum = GetDigitSum(_inNum);
-            _inNum = 10 * Get2nd(_inNum) + Get2nd(digitSum);
-            // std::cout << _inNum << "\n";
-            if (_inNum == origin) {
-                std::cout << counter << "\n";
-                return counter;
-            }
-        }
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-        // std::cout << "Bye...\n";
-        return -1;
+NodeList *ReverseList(NodeList *pHead) {
+    NodeList *pList = pHead;
+
+    NodeList *previous = NULL;
+    while (pList) {
+        NodeList *next = pList->next;
+        pList->next = previous;
+        previous = pList;
+
+        pList = next;
     }
 
+    return previous;
 }
 
 
+int main() {
+    std::vector<int> inArr {5, 4, 2, 6, 1, -6, 8, 9, 4};
 
+    NodeList *pNodeList = NULL;
+
+    pNodeList = new NodeList();
+    NodeList *ROOT = pNodeList;
+    for (int i = 0; i < inArr.size(); ++i) {
+        pNodeList->next = new NodeList(inArr[i]);
+        pNodeList = pNodeList->next;
+    }
+
+    pNodeList = ROOT->next;
+    while (pNodeList) {
+        std::cout << pNodeList->data << " ";
+        pNodeList = pNodeList->next;
+    }
+    std::cout << "\n";
+
+    NodeList *newRoot = ReverseList(ROOT);
+    pNodeList = newRoot->next;
+    while (pNodeList) {
+        std::cout << pNodeList->data << " ";
+        pNodeList = pNodeList->next;
+    }
+
+    std::cout << "\n";
+    return 0;
+}
 

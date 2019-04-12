e3a8a0ff4c26800e83345b31d91f9810f5596c8c
dunwenqiang <dunwenqiang@gmail.com>
2019-04-12 11:32:19 +0800

    【修改】快递问题，减少时间复杂度的。

--- SmartCollect/src_branch_tmi/tools/test.cc
+++ SmartCollect/src_branch_tmi/tools/test.cc
@@ -2,9 +2,9 @@
 // 10 15 12
 // 1 9 12 23 26 37
 
-
 #include <iostream>
 #include <vector>
+#include <map>
 
 void GetBoundary(const std::vector<int> &numOfRooms, std::vector<int> &boundaries) {
     int sum = 0;
@@ -14,45 +14,49 @@ void GetBoundary(const std::vector<int> &numOfRooms, std::vector<int> &boundarie
     }
 }
 
+struct BuildingIdAndRoomId {
+    int buildingId_;
+    int roomId_;
 
-
-
-int GetBuildingId(int roomId, const std::vector<int> &boundaries) {
-    if (roomId > boundaries.back()) {
-        exit(1);
+    BuildingIdAndRoomId(): buildingId_(-1), roomId_(-1) {
     }
-
-    for (int i = 0; i < boundaries.size(); ++i) {
-        if (roomId < boundaries[i]) {
-            return (i + 1);
-        }
-    }
-    return 0;
-}
+};
 
 int main() {
-    std::vector<int> numOfRooms {10, 15, 12};
-    std::vector<int> boundaries;
-    boundaries.clear();
+    const std::vector<int> numOfRooms {10, 15, 12};
+    const std::vector<int> roomId {7, 0, 1, 9, -12, 23, 26, 88, 45};
+
+    BuildingIdAndRoomId buildingIdAndRoomId;
+    std::map<int, BuildingIdAndRoomId> buildingIdAndRoomIdMap;
+    for (auto i: roomId) {
+        buildingIdAndRoomIdMap.insert({i, buildingIdAndRoomId});
+    }
+
+    std::vector<int> boundaries {0};
     GetBoundary(numOfRooms, boundaries);
 
-    for (auto i: boundaries) {
-        std::cout << i << " ";
-    }
+    auto mapIter = buildingIdAndRoomIdMap.begin();
+    for (int buildingId = 1; buildingId != boundaries.size(); ++buildingId) {
+        for ( ; mapIter != buildingIdAndRoomIdMap.end(); ++mapIter) {
+            if (mapIter->first <= 0) {
+                continue;
+            }
+            if (mapIter->first > boundaries[buildingId]) {
+                break;
+            }
 
-    std::vector<int> roomId {37};
-    for (int i = 0; i < roomId.size(); ++i) {
-        int buildingId = GetBuildingId(roomId[i], boundaries);
-        std::cout << roomId[i] << ": " << buildingId << "\n";
-        int roomIdOfBuilding = -1;
-        if (1 == buildingId) {
-            roomIdOfBuilding = roomId[i];
+            // std::cout << mapIter->first << " is in " << buildingId << " building, " << mapIter->first - boundaries[buildingId - 1] << " room.\n";
+            mapIter->second.buildingId_ = buildingId;
+            mapIter->second.roomId_ = mapIter->first - boundaries[buildingId - 1];
         }
-        roomIdOfBuilding = roomId[i] - boundaries[buildingId - 1];
-
-        std::cout << roomId[i] << " is " <<  roomIdOfBuilding << " roomIdOfBuilding; " << roomIdOfBuilding  << " roomIdOfBuilding.\n";
     }
 
+    for (auto i: buildingIdAndRoomIdMap) {
+        if (i.second.buildingId_ < 0) {
+            std::cout << "Illegal room " << i.first << "\n";
+            continue;
+        }
+        std::cout << i.first << " is in " << i.second.buildingId_ << " building, " << i.second.roomId_ << " room.\n";
+    }
 }
-
 

2ed68e6283b4da156528933a7b866b83c8a2c2ff
dunwenqiang <dunwenqiang@gmail.com>
2019-07-01 19:51:56 +0800

    【删除】不用的分支代码。

--- SmartCollect/src/tools_slam_wavelet/collectedDataFixed/mkMd5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-absolute_script_path=$(cd $(dirname $0) && pwd)
-echo "${absolute_script_path}"
--- SmartCollect/src/tools_slam_wavelet/monitor.sh
+++
@@ -1,5 +0,0 @@
-
-newest_rawdata=$(ls -t /opt/smartc/record/ | head -n1)
-absolute_newest="/opt/smartc/record/${newest_rawdata}/Rawdata/"
-echo "Watching ${absolute_newest}..."
-watch -n1 -d "ls -l ${absolute_newest}/Lidar/lidar.dat; ls -l ${absolute_newest}/IMU; ls -l ${absolute_newest}/Image | wc -l;"

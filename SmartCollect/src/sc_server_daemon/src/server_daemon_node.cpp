e2167ed97bbcf527d07f210632eb087eea67ff8f
dunwenqiang <dunwenqiang@gmail.com>
2019-07-02 11:17:47 +0800

    【新增】使用工厂模式重构服务器节点。

--- SmartCollect/src/sc_server_daemon/src/server_daemon_node.cpp
+++ SmartCollect/src/sc_server_daemon/src/server_daemon_node.cpp
@@ -1,18 +1,25 @@
-#include "server_daemon.h"
+#include <glog/logging.h>
+#include <ros/ros.h>
+#include "server_factory.h"
+#include "base_server.h"
 
 int main(int argc, char **argv) {
     // server node glog path:
     FLAGS_log_dir = "/opt/smartc/log";
     google::InitGoogleLogging(argv[0]);
-    LOG(INFO) << "Got " << argc << " parameters.";
-
     ros::init(argc, argv, "server_daemon_node");
 
-    ServerDaemon serverDaemoner;
-    if (2 == argc) {
-        serverDaemoner.UpdateProjectInfo(argv[1]);
-    }
+    ServerFactory serverFactory;
+    serverFactory.SetSensorType("/opt/smartc/config/sensor_type.yaml");
+    BaseServer *pServer = serverFactory.CreateServer();
 
-    serverDaemoner.Run();
+    LOG(INFO) << "Got " << argc << " parameters.";
+    if (2 == argc) {
+        pServer->UpdateProjectInfo(argv[1]);
+    }
+    pServer->Run();
+
+    delete pServer;
+    pServer = NULL;
     return 0;
 }
